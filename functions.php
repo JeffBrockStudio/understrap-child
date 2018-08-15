4695c718281ac85b028d4cfb83a24ca8fcea4b7a
JeffBrockStudio <jeff@jeffbrockstudio.com>
2018-08-15 15:03:24 -0700

    Added Slugify function.

--- functions.php
+++ functions.php
@@ -158,3 +158,33 @@ function understrap_mime_types($mimes) {
   return $mimes;
 }
 add_filter('upload_mimes', 'understrap_mime_types');
+
+
+/**
+ * Convert text to a slug
+ */
+function slugify($text) {
+  // replace non letter or digits by -
+  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+  // remove unwanted characters
+  $text = preg_replace('~[^-\w]+~', '', $text);
+
+  // trim
+  $text = trim($text, '-');
+
+  // remove duplicate -
+  $text = preg_replace('~-+~', '-', $text);
+
+  // lowercase
+  $text = strtolower($text);
+
+  if (empty($text)) {
+    return 'n-a';
+  }
+
+  return $text;
+}
