9b70426d95e3c100979f0f408efa11fd33935628
JeffBrockStudio <jeff@jeffbrockstudio.com>
2019-02-01 17:04:23 -0800

    Added video wrapper CSS utililty. Added jQuery window resize.

---
+++ src/js/custom-javascript.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+});
+
+
+jQuery(function($) {
+  $(window).on('resize', function() {	  
+/*
+		if ( Modernizr.mq('(min-width: 768px)')) {
+		}
+*/
+		
+  }).trigger('resize');
+});
