009da8fcee78f54f42c791b2f51f51b279de7894
JeffBrockStudio <jeff@jeffbrockstudio.com>
2022-11-06 16:14:41 -0800

    Flipped layers in Reveal.

--- inc/blocks/reveal.php
+++ inc/blocks/reveal.php
@@ -3,41 +3,8 @@
 <div class="sticky">
 	<div class="wipe-stack">
 	
-	<div class="layer bottom" aria-hidden="true">
-		<div class="container"> 
-			<div class="row">
-				
-				<div class="col-12">
-					
-					<?php if ( get_sub_field( 'bottom_layer_heading_superheading' )): ?>
-						<div role="doc-subtitle" class="superheading"><?php the_sub_field( 'bottom_layer_heading_superheading' );?></div>
-					<?php endif; ?>
-					
-					<<?php the_sub_field( 'bottom_layer_heading_heading_level' );?> class="heading"><?php the_sub_field( 'bottom_layer_heading_heading' );?></<?php the_sub_field( 'bottom_layer_heading_heading_level' );?>>
-					
-					<?php if ( get_sub_field( 'bottom_layer_heading_subheading' )): ?>
-						<div role="doc-subtitle" class="subheading" style="color: <?php echo $text_color; ?>"><?php the_sub_field( 'bottom_layer_heading_subheading' );?></div>
-					<?php endif; ?>
-					
-				</div>																	
-			</div>
-			
-		</div>
-		
-		<?php if ( get_sub_field( 'bottom_layer_background_video' )):?>
-				<div class="background video" style="background-image:url(<?php $image = get_sub_field( 'bottom_layer_background_image'); echo $image['url']; ?>)";>
-						<video id="home-featured-video" src="<?php $video_file = get_sub_field( 'bottom_layer_background_video' ); echo $video_file['url']; ?>" playsinline autoplay loop muted style="background: url(<?php echo $image['url'];  ?>); background-size: cover;" /></video>						
-						<!-- <div class="video-controls"><i class="fas fa-pause"></i></div> -->
-				</div>
-			<?php
-		else: ?>
-				<div class="background image" style="background-image:url(<?php $image = get_sub_field( 'bottom_layer_background_image'); echo $image['url'];?>)"></div>		
-		<?php
-		endif;
-		?>
-	</div>		
-		<div class="layer top">
-			<div class="container">
+		<div class="layer bottom" aria-hidden="true">
+			<div class="container"> 
 				<div class="row">
 					
 					<div class="col-12">
@@ -46,7 +13,7 @@
 							<div role="doc-subtitle" class="superheading"><?php the_sub_field( 'top_layer_heading_superheading' );?></div>
 						<?php endif; ?>
 						
-						<<?php the_sub_field( 'top_layer_heading_heading_level' );?> class="heading" style="color: <?php // echo $text_color; ?>"><?php the_sub_field( 'top_layer_heading_heading' );?></<?php the_sub_field( 'top_layer_heading_heading_level' );?>>
+						<<?php the_sub_field( 'top_layer_heading_heading_level' );?> class="heading"><?php the_sub_field( 'top_layer_heading_heading' );?></<?php the_sub_field( 'top_layer_heading_heading_level' );?>>
 						
 						<?php if ( get_sub_field( 'top_layer_heading_subheading' )): ?>
 							<div role="doc-subtitle" class="subheading" style="color: <?php echo $text_color; ?>"><?php the_sub_field( 'top_layer_heading_subheading' );?></div>
@@ -64,13 +31,45 @@
 					</div>
 				<?php
 			else: ?>
-					<div class="background image" style="background-image:url(<?php $image = get_sub_field( 'top_layer_background_image'); echo $image['url'];?>)"></div>		
+					<div class="background image" style="background-image:url(<?php $image = get_sub_field( 'bottom_layer_background_image'); echo $image['url'];?>)"></div>		
+			<?php
+			endif;
+			?>
+		</div>		
+
+		<div class="layer top">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-12">
+						
+						<?php if ( get_sub_field( 'bottom_layer_heading_superheading' )): ?>
+							<div role="doc-subtitle" class="superheading"><?php the_sub_field( 'bottom_layer_heading_superheading' );?></div>
+						<?php endif; ?>
+						
+						<<?php the_sub_field( 'bottom_layer_heading_heading_level' );?> class="heading" style="color: <?php // echo $text_color; ?>"><?php the_sub_field( 'bottom_layer_heading_heading' );?></<?php the_sub_field( 'bottom_layer_heading_heading_level' );?>>
+						
+						<?php if ( get_sub_field( 'bottom_layer_heading_subheading' )): ?>
+							<div role="doc-subtitle" class="subheading" style="color: <?php echo $text_color; ?>"><?php the_sub_field( 'top_layer_heading_subheading' );?></div>
+						<?php endif; ?>
+						
+					</div>																	
+				</div>
+				
+			</div>
+			
+			<?php if ( get_sub_field( 'bottom_layer_background_video' )):?>
+					<div class="background video" style="background-image:url(<?php $image = get_sub_field( 'bottom_layer_background_image'); echo $image['url']; ?>)";>
+							<video id="home-featured-video" src="<?php $video_file = get_sub_field( 'bottom_layer_background_video' ); echo $video_file['url']; ?>" playsinline autoplay loop muted style="background: url(<?php echo $image['url'];  ?>); background-size: cover;" /></video>						
+							<!-- <div class="video-controls"><i class="fas fa-pause"></i></div> -->
+					</div>
+				<?php
+			else: ?>
+					<div class="background image" style="background-image:url(<?php $image = get_sub_field( 'bottom_layer_background_image'); echo $image['url'];?>)"></div>		
 			<?php
 			endif;
 			?>
 		</div>
-		
-	
 		
 	</div>
 </div>
