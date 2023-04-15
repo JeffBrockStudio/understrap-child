c861412d43ac5e21f0277b44b7fdf714f84eb1ac
JeffBrockStudio <jeff@jeffbrockstudio.com>
2023-04-14 22:40:24 -0700

    Started manually merging upstream changes.

--- src/build/rollup.config.js
+++ src/build/rollup.config.js
@@ -1,58 +1,79 @@
-'use strict'
+'use strict';
 
-const path = require('path')
-const { babel } = require('@rollup/plugin-babel')
-const { nodeResolve } = require('@rollup/plugin-node-resolve')
-import commonjs from '@rollup/plugin-commonjs'
-import multi from '@rollup/plugin-multi-entry'
-const replace = require('@rollup/plugin-replace')
-const banner = require('./banner.js')
+/**
+ * External dependencies
+ */
+const path = require( 'path' );
+const { babel } = require( '@rollup/plugin-babel' );
+const { nodeResolve } = require( '@rollup/plugin-node-resolve' );
+const commonjs = require( '@rollup/plugin-commonjs' );
+const multi = require( '@rollup/plugin-multi-entry' );
+const replace = require( '@rollup/plugin-replace' );
 
-let fileDest = 'child-theme.js'
-const external = ['jquery']
+/**
+ * Internal dependencies
+ */
+const banner = require( './banner.js' );
+
+// Populate Bootstrap version specific variables.
+let bsVersion = 5;
+let bsSrcFile = 'bootstrap.js';
+let fileDest = 'child-theme';
+let globals = {
+	jquery: 'jQuery', // Ensure we use jQuery which is always available even in noConflict mode
+	'@popperjs/core': 'Popper',
+};
+
+const external = [ 'jquery' ];
+
 const plugins = [
-  babel({
-    // Only transpile our source code
-    exclude: 'node_modules/**',
-    // Include the helpers in the bundle, at most one copy of each
-    babelHelpers: 'bundled'
-  }),
-  replace({
-      'process.env.NODE_ENV': '"production"',
-      preventAssignment: true
-  }),
-  nodeResolve(),
-  commonjs(),
-  multi()
-]
-const globals = {
-  jquery: 'jQuery', // Ensure we use jQuery which is always available even in noConflict mode
-}
-
+	babel( {
+		browserslistEnv: `bs${ bsVersion }`,
+		// Include the helpers in the bundle, at most one copy of each.
+		babelHelpers: 'bundled',
+	} ),
+	replace( {
+		'process.env.NODE_ENV': '"production"',
+		preventAssignment: true,
+	} ),
+	nodeResolve(),
+	commonjs(),
+	multi(),
+];
 
 module.exports = {
   input: [
-    path.resolve(__dirname, '../js/bootstrap.js'), 
-    path.resolve(__dirname, '../js/skip-link-focus-fix.js'), 
-    path.resolve(__dirname, '../js/jquery.gridder.min.js'),       // Gridder
-    path.resolve(__dirname, '../js/imagesloaded.pkgd.min.js'),    // Imagesloaded
-    path.resolve(__dirname, '../js/lazysizes.min.js'),            // Lazysizes
-    path.resolve(__dirname, '../js/jquery.matchHeight-min.js'),   // MatchHeight
-    path.resolve(__dirname, '../js/modernizer-custom.js'),        // Modernizr
-    path.resolve(__dirname, '../js/parallax.min.js'),             // Parallax
-    path.resolve(__dirname, '../js/jquery.prettydropdowns.js'),   // Pretty Dropdowns
-    path.resolve(__dirname, '../js/slick.min.js'),                // Slick Carousel
-    path.resolve(__dirname, '../js/smooth-scroll.min.js'),        // Smooth Scroll    
-    path.resolve(__dirname, '../js/jquery.sticky.js'),            // Sticky
-    path.resolve(__dirname, '../js/jquery.touchSwipe.min.js')     // Touchswipe
+		path.resolve( __dirname, `../js/${ bsSrcFile }` ),
+		path.resolve( __dirname, '../js/skip-link-focus-fix.js' ),
+		path.resolve( __dirname, '../js/custom-javascript.js' ),
+    path.resolve( __dirname, '../js/jquery.gridder.min.js' ),       // Gridder
+    path.resolve( __dirname, '../js/imagesloaded.pkgd.min.js' ),    // Imagesloaded
+    path.resolve( __dirname, '../js/lazysizes.min.js' ),            // Lazysizes
+    path.resolve( __dirname, '../js/jquery.matchHeight-min.js' ),   // MatchHeight
+    path.resolve( __dirname, '../js/modernizer-custom.js' ),        // Modernizr
+    path.resolve( __dirname, '../js/parallax.min.js' ),             // Parallax
+    path.resolve( __dirname, '../js/jquery.prettydropdowns.js' ),   // Pretty Dropdowns
+    path.resolve( __dirname, '../js/slick.min.js' ),                // Slick Carousel
+    path.resolve( __dirname, '../js/smooth-scroll.min.js' ),        // Smooth Scroll    
+    path.resolve( __dirname, '../js/jquery.sticky.js' ),            // Sticky
+    path.resolve( __dirname, '../js/jquery.touchSwipe.min.js' ),    // Touchswipe
   ],
-  output: {
-    banner,
-    file: path.resolve(__dirname, `../../js/${fileDest}`),
-    format: 'umd',
-    globals,
-    name: 'understrap'
-  },
-  external,
-  plugins
-}
+	output: [
+		{
+			banner: banner(''),
+			file: path.resolve( __dirname, `../../js/${ fileDest }.js` ),
+			format: 'umd',
+			globals,
+			name: 'understrap',
+		},
+		{
+			banner: banner(''),
+			file: path.resolve( __dirname, `../../js/${ fileDest }.min.js` ),
+			format: 'umd',
+			globals,
+			name: 'understrap',
+		},
+	],
+	external,
+	plugins,
+};
