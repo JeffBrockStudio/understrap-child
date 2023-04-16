95d608b99494100add44eb2480ff52be99ba1e03
JeffBrockStudio <jeff@jeffbrockstudio.com>
2023-04-15 23:33:46 -0700

    Remove auto featured image on Gutenberg pages.

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -11,97 +12,99 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 
 ?>
 
 <div class="wrapper" id="page-wrapper">
-		
+
 	<?php
 	// Check if any blocks are present
-	if ( is_home()) {
-		$post_id = get_option( 'page_for_posts' );
+	if (is_home()) {
+		$post_id = get_option('page_for_posts');
 	} else {
 		$post_id = $post->ID;
 	}
-	
-	
-	if( have_rows('blocks', $post_id) ): 
-		// Show ACF blocks ?>
+
+
+	if (have_rows('blocks', $post_id)) :
+		// Show ACF blocks 
+	?>
 		<main class="site-main" id="content">
-			
-			<?php while ( have_posts() ) : the_post(); ?>
-				
-				<?php if( !post_password_required( $post )): ?>
-					<?php require( 'inc/blocks.php' ); ?>
+
+			<?php while (have_posts()) : the_post(); ?>
+
+				<?php if (!post_password_required($post)) : ?>
+					<?php require('inc/blocks.php'); ?>
 				<?php endif; ?>
-	
+
 			<?php endwhile; ?>
-	
+
 		</main>
-		
-		<?php	
-	elseif ( has_blocks( $post_id ) ) :
-		// Show Gutenberg blocks ?>
-		<main class="site-main" id="content">
+
+	<?php
+	elseif (has_blocks($post_id)) :
+		// Show Gutenberg blocks 
+	?>
+		<main class="site-main has-blocks" id="content">
 			<?php
-			while ( have_posts() ) {
+			while (have_posts()) {
 				the_post();
-				get_template_part( 'loop-templates/content', 'page' );
+				the_content();
 
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
+				if (comments_open() || get_comments_number()) {
 					comments_template();
 				}
 			} ?>
 		</main>
-		<?php
-	else:
+	<?php
+	else :
 	?>
-	
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-		<div class="row">
+		<div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+			<div class="row">
 
-			<main class="site-main" id="main">
+				<!-- Do the left sidebar check -->
+				<?php get_template_part('global-templates/left-sidebar-check'); ?>
 
-				<?php
-				while ( have_posts() ) {
-					the_post();
-					get_template_part( 'loop-templates/content', 'page' );
+				<main class="site-main" id="main">
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
+					<?php
+					while (have_posts()) {
+						the_post();
+						get_template_part('loop-templates/content', 'page');
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if (comments_open() || get_comments_number()) {
+							comments_template();
+						}
 					}
-				}
-				?>
+					?>
 
-			</main><!-- #main -->
+				</main><!-- #main -->
 
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+				<!-- Do the right sidebar check -->
+				<?php get_template_part('global-templates/right-sidebar-check'); ?>
 
-		</div><!-- .row -->
+			</div><!-- .row -->
 
-	</div><!-- #content -->
-	
+		</div><!-- #content -->
+
 	<?php
 	endif;
 	?>
 
 </div><!-- #page-wrapper -->
 
-<?php 
-if ( get_field( 'vertical_accordion_menu' )):	
-	require( 'inc/vertical-accordion-menu.php' );	
+<?php
+if (get_field('vertical_accordion_menu')) :
+	require('inc/vertical-accordion-menu.php');
 endif;
 
 get_footer();
