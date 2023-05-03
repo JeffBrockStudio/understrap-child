5f64d4d18e9dee34f847222069524d6a0175a1f9
JeffBrockStudio <jeff@jeffbrockstudio.com>
2023-05-03 13:14:35 -0700

    Testing patterns.

---
+++ patterns/test-pattern.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Test Pattern
+ * Slug: test-pattern
+ * Description: 
+ * Categories: 
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: 
+ * Post Types: page
+ * Inserter: true
+ */
+
+?>
+<!-- wp:gallery {"linkTo":"none","align":"wide"} -->
+<figure class="wp-block-gallery alignwide has-nested-images columns-default is-cropped"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/nature-above-01.jpg" alt="An aerial view of waves crashing against a shore."/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/nature-above-02.jpg" alt="An aerial view of a field. A road runs through the upper right corner."/></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery -->
+
+<!-- wp:video -->
+<figure class="wp-block-video"></figure>
+<!-- /wp:video -->
+
+<!-- wp:paragraph -->
+<p>Video caption here.</p>
+<!-- /wp:paragraph -->
